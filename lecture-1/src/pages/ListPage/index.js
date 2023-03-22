7a5368786371de7f20cfc4d97acad4ead9453ff6
younjaewon <97930034+younjaewon@users.noreply.github.com>
2023-03-23 00:37:59 +0900

    git 캐시 파일 삭제

---
+++ lecture-1/src/pages/ListPage/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./index.css";
+import BasicTemplates from "../../templates/BasicTemplates";
+import ArticleList from "../../components/ArticleList";
+
+function ListPage(props) {
+  return (
+    <BasicTemplates>
+      gdgg
+      <div style={{ width: "700px", margin: "auto" }}>
+        <ArticleList />
+      </div>
+    </BasicTemplates>
+  );
+}
+
+export default ListPage;
